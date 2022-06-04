4abc126172c0014dd1514c21270171887b4f0e8d
Maria <maria.carranzavaldes@gmail.com>
2022-06-04 23:06:25 +0200

    footer terminado

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,27 @@
+import { Box } from "@mui/system";
 import React from "react";
 import fb from '../images/facebook.svg';
 import tw from '../images/twitter.svg';
 
 
+
 function Footer() {
     return(
-        <section id="footer" style={{display:'flex', flexDirection:'row', justifyContent:'space-around', alignItems:'center' }}>
-            <a href='#' style={{color:'#FFFFF5'}}>About us</a>
-            <a href='#' style={{color:'#FFFFF5'}}>Contact</a>
-            <img src={fb} alt='facebook'/>
+        <section id="footer" style={{background:'#202027', display:'flex', flexDirection:'row', justifyContent:'space-around', alignItems:'center', fontWeight: 'bold', color:'#FFFFF5'}}>
+            <Box>
+            <a href='#' style={{color:'#FFFFF5', paddingRight: '0.8rem', textDecoration: 'none'}}>About us </a>
+            <a href='#' style={{color:'#FFFFF5', textDecoration: 'none'}}>Contact</a>
+            </Box>
+            <Box style={{display:'flex'}}>
+            <Box style={{display:'flex'}}>
+            <img src={tw} alt='twitter' style={{paddingRight: '0.5rem'}}/>
+            <p style={{paddingRight: '0.8rem'}}>Twitter</p>
+            </Box>
+            <Box style={{display:'flex'}}>
+            <img src={fb} alt='facebook' style={{paddingRight: "0.5rem"}}/>
             <p>Facebook</p>
-            <img src={tw} alt='twitter'/>
-            <p>Twitter</p>
+            </Box>
+            </Box>
         </section>
       
     )
